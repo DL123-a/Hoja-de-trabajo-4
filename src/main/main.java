cf10c88e8aa6e47f6968639813780f97f85e3356
DL123-a <dannylealgt@gmail.com>
2026-02-27 23:15:00 -0600

    Primera version main

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,31 @@
+package main;
 
+import adtstack.Stack;
+import adtstack.VectorStack;
+import calculadora.InfixPostfix;
+import calculadora.PostfixEval;
 
-public class main {
-    
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Crear stack manualmente (por ahora fijo)
+        Stack<Integer> testStack = new VectorStack<>();
+
+        testStack.push(10);
+        testStack.push(20);
+
+        System.out.println("Prueba Stack pop: " + testStack.pop());
+
+        // Expresion de prueba
+        String infix = "(10+20)*9";
+
+        // Convertir a postfix
+        String postfix = InfixPostfix.convert(infix);
+        System.out.println("Postfix: " + postfix);
+
+        // Evaluar postfix
+        int result = PostfixEval.evaluate(postfix);
+        System.out.println("Resultado: " + result);
+    }
 }
