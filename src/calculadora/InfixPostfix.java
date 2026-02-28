abb33616e56fb1e975ae80a8f0696ed271c4ef1c
DL123-a <dannylealgt@gmail.com>
2026-02-27 23:12:42 -0600

    Compilaciones

--- src/calculadora/InfixPostfix.java
+++ src/calculadora/InfixPostfix.java
@@ -1,5 +1,57 @@
+package calculadora;
 
+import adtstack.Stack;
+import adtstack.VectorStack;
 
 public class InfixPostfix {
-    
+
+    public static String convert(String infix) {
+
+        Stack<Character> stack = new VectorStack<>();
+        StringBuilder postfix = new StringBuilder();
+
+        for (int i = 0; i < infix.length(); i++) {
+
+            char ch = infix.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                postfix.append(ch).append(" ");
+            }
+
+            else if (ch == '(') {
+                stack.push(ch);
+            }
+
+            else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.pop(); // quitar '('
+            }
+
+            else if (isOperator(ch)) {
+                while (!stack.isEmpty() &&
+                       precedence(ch) <= precedence(stack.peek())) {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.push(ch);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop()).append(" ");
+        }
+
+        return postfix.toString().trim();
+    }
+
+    private static boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+
+    private static int precedence(char ch) {
+        if (ch == '+' || ch == '-') return 1;
+        if (ch == '*' || ch == '/') return 2;
+        return 0;
+    }
 }
--- src/calculadora/PostfixEval.java
+++ src/calculadora/PostfixEval.java
@@ -1,5 +1,42 @@
 package calculadora;
 
+import adtstack.Stack;
+import adtstack.VectorStack;
+
 public class PostfixEval {
-    
+
+    public static int evaluate(String postfix) {
+
+        Stack<Integer> stack = new VectorStack<>();
+
+        String[] tokens = postfix.split(" ");
+
+        for (String token : tokens) {
+
+            if (token.matches("\\d+")) {
+                stack.push(Integer.parseInt(token));
+            } else {
+
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
 }
